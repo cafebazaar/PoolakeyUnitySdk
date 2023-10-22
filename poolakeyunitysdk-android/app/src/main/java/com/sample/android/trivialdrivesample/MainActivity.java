e7781c8b53e68050afbf75a2dae861dd6145ea03
Seyyed davud hosseiny <hosseinydavid@gmail.com>
2023-10-22 17:57:24 +0330

    Move some code to App class

--- poolakeyunitysdk-android/app/src/main/java/com/sample/android/trivialdrivesample/MainActivity.java
+++ poolakeyunitysdk-android/app/src/main/java/com/sample/android/trivialdrivesample/MainActivity.java
@@ -10,16 +10,8 @@ import android.widget.TextView;
 import com.farsitel.bazaar.PaymentActivity;
 import com.farsitel.bazaar.PoolakeyBridge;
 import com.farsitel.bazaar.PoolakeyKotlinBridge;
-import com.farsitel.bazaar.callback.ConnectionCallback;
 import com.farsitel.bazaar.callback.ConsumeCallback;
 import com.farsitel.bazaar.callback.PaymentCallback;
-import com.farsitel.bazaar.callback.PurchasesCallback;
-import com.farsitel.bazaar.callback.SKUDetailsCallback;
-
-import java.util.Arrays;
-import java.util.List;
-
-import ir.cafebazaar.poolakey.entity.SkuDetails;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -30,61 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-
-        PoolakeyKotlinBridge.INSTANCE.connect(getApplicationContext(), null, new ConnectionCallback() {
-            @Override
-            public void onConnect() {
-                log("onConnect");
-                getSKUDetails();
-            }
-
-            @Override
-            public void onDisconnect() {
-                log("onDisconnect");
-            }
-
-            @Override
-            public void onFailure(String message, String stackTrace) {
-                log("onFailure " + message);
-            }
-        });
-    }
-
-    private void getSKUDetails() {
-        PoolakeyKotlinBridge.INSTANCE.getSkuDetails("inapp", Arrays.asList("gas,premium,trial_subscription,infinite_gas_monthly,dynamic_price".split(",")), new SKUDetailsCallback() {
-            @Override
-            public void onSuccess(List<SkuDetails> data) {
-                log("onSkuDetailsSuccess " + data.toString());
-            }
-
-            @Override
-            public void onFailure(String message, String stackTrace) {
-                log("onSkuDetailsFailure " + message);
-            }
-        });
+        ((App) getApplication()).getLiveData().observe(this, this::log);
     }
 
     public void purchase(View view) {
         PoolakeyKotlinBridge.INSTANCE.startActivity(this, PaymentActivity.Command.PurchaseProduct, new PaymentCallback() {
             @Override
             public void onStart() {
-                log("onPurchaseStart");
+                ((App) getApplication()).log("onPurchaseStart");
             }
 
             @Override
             public void onCancel() {
-                log("onPurchaseCancel");
+                ((App) getApplication()).log("onPurchaseCancel");
             }
 
             @Override
             public void onSuccess(String orderId, String purchaseToken, String payload, String packageName, int purchaseState, long purchaseTime, String productId, String originalJson, String dataSignature) {
-                log("onPurchaseSuccess " + orderId);
+                ((App) getApplication()).log("onPurchaseSuccess " + orderId);
                 consume(purchaseToken);
             }
 
             @Override
             public void onFailure(String message, String stackTrace) {
-                log("onPurchaseFailure " + message);
+                ((App) getApplication()).log("onPurchaseFailure " + message);
             }
         }, "gas", "", "");
     }
@@ -93,12 +54,12 @@ public class MainActivity extends AppCompatActivity {
         PoolakeyKotlinBridge.INSTANCE.consume(purchaseToken, new ConsumeCallback() {
             @Override
             public void onSuccess() {
-                log("onConsumeSuccess");
+                ((App) getApplication()).log("onConsumeSuccess");
             }
 
             @Override
             public void onFailure(String message, String stackTrace) {
-                log("onConsumeFailure " + message);
+                ((App) getApplication()).log("onConsumeFailure " + message);
 
             }
         });
@@ -106,6 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
     void log(String message) {
         Log.i(PoolakeyBridge.TAG, message);
-        textView.append(message + "\n");
+        textView.setText(message);
     }
 }
